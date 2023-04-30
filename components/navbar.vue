04603320ed812ad9c1ea9126ed4408adaccda259
unknown <hovahyii@gmail.com>
2023-05-01 04:14:15 +0800

    update all pages
    Signed-off-by: unknown <hovahyii@gmail.com>

--- components/navbar.vue
+++ components/navbar.vue
@@ -11,15 +11,40 @@
 			Events
 		</v-btn>
 
-		<v-btn to="/committee">
+		<v-btn to="/committees" prepend-icon="mdi-account" stacked>
+			Committees
+		</v-btn>
+
+         <v-btn to="/forum">
 			<v-icon>mdi-account-group</v-icon>
 			Forum
 		</v-btn>
 
 		<v-btn to="/announcement">
 			<v-icon>mdi-bullhorn</v-icon>
-			News
+			Announcement
 		</v-btn>
 
 	</v-bottom-navigation>
 </template>
+
+
+<style scoped>
+  /* hide certain characters on mobile screens */
+  @media only screen and (max-width: 600px) {
+    .v-btn__content {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+  }
+  
+  /* show full text on desktop screens */
+  @media only screen and (min-width: 601px) {
+    .v-btn__content {
+      overflow: visible;
+      text-overflow: clip;
+      white-space: normal;
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,7 @@
             Engineering Week is an annual event that celebrates the field of engineering and its contributions to society. Join us for a week of exciting events and activities that showcase the innovative and creative side of engineering!
           </v-card-text>
         </v-card>
+       
       </v-col>
     </v-row>
   </v-container>
