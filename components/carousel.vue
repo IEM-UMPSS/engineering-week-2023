e18c2115f0e6cd9806ef86c4d7f0c17a0e5d149c
Zhaolin Lau <zhaolinlau@gmail.com>
2023-05-01 08:14:04 +0800

    modify width of carousel and refactor code quality

--- components/carousel.vue
+++ components/carousel.vue
@@ -1,36 +1,20 @@
 <template>
-  <v-carousel
-    cycle
-    height="250"
-    hide-delimiter-background
-    show-arrows="hover"
-    max-width="800"
-  >
-    <v-carousel-item
-      v-for="(slide, i) in slides"
-      :key="i"
-    >
-        <v-img :src="slide.src" :contain="true" class="mx-auto" max-width="800" cover></v-img>
-    </v-carousel-item>
-  </v-carousel>
+	<v-carousel cycle show-arrows="hover" progress="success" color="teal">
+		<v-carousel-item v-for="(slide, i) in slides" :src="slide.src" :key="i"></v-carousel-item>
+	</v-carousel>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      slides: [
-         {
-            src: '/slides/1.jpg',
-          },
-         {
-            src: '/slides/2.jpg',
-          },
-          {
-            src: '/slides/3.jpg',
-          },
-      
-      ],
-    }
-  },
-}
+
+<script setup>
+const slides = [
+	{
+		src: '/slides/1.jpg',
+	},
+	{
+		src: '/slides/2.jpg',
+	},
+	{
+		src: '/slides/3.jpg',
+	},
+]
+
 </script>
