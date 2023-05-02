0fd16ffe31e4c91cfdfe62ced6f055793dff1514
Zhaolin Lau <zhaolinlau@gmail.com>
2023-05-02 12:58:36 +0800

    refactor navbars

--- app.vue
+++ app.vue
@@ -1,8 +1,7 @@
 <template>
 	<v-card>
 		<v-app>
-			<v-app-bar>
-				<v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+			<v-app-bar density="compact" scroll-behavior="hide">
 				<v-toolbar-title>Engineering Week</v-toolbar-title>
 				<template v-slot:append>
 					<v-btn @click="toggleTheme"
@@ -10,21 +9,14 @@
 				</template>
 			</v-app-bar>
 
-			<v-navigation-drawer v-model="drawer" temporary>
-				<v-list color="teal">
-					<v-list-item to="/" prepend-icon="mdi-home">Home</v-list-item>
-					<v-list-item to="/events" prepend-icon="mdi-calendar-month">Events</v-list-item>
-					<v-list-item to="/committees" prepend-icon="mdi-account">Committees</v-list-item>
-					<v-list-item to="/forum" prepend-icon="mdi-account-group">Forum</v-list-item>
-					<v-list-item to="/announcement" prepend-icon="mdi-bullhorn">Announcement</v-list-item>
-				</v-list>
-			</v-navigation-drawer>
-
 			<v-main>
 				<v-container fluid>
 					<RouterView />
+					<Footer />
 				</v-container>
 			</v-main>
+
+			<botnav />
 		</v-app>
 	</v-card>
 </template>
@@ -32,16 +24,10 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import { useTheme } from 'vuetify'
-import { ref, watch } from 'vue'
-
-const drawer = ref(false)
-const group = ref(null)
+import Footer from './components/footer.vue'
+import botnav from './components/botnav.vue'
 
 const toggleTheme = () => theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
 const theme = useTheme()
-
-watch(group, () => {
-	drawer.value = false
-})
 
 </script>
---
+++ components/botnav.vue
@@ -0,0 +1,11 @@
+<template>
+	<v-app-bar location="bottom" density="compact">
+		<v-tabs color="teal" centered stacked grow>
+					<v-tab to="/" prepend-icon="mdi-home">Home</v-tab>
+					<v-tab to="/events" prepend-icon="mdi-calendar-month">Events</v-tab>
+					<v-tab to="/committees" prepend-icon="mdi-account">Committees</v-tab>
+					<v-tab to="/forum" prepend-icon="mdi-account-group">Forum</v-tab>
+					<v-tab to="/announcement" prepend-icon="mdi-bullhorn">Announcement</v-tab>
+				</v-tabs>
+	</v-app-bar>
+</template>
